e3b21f267b0e4d5f51e516f13add9e16c37a1943
Leonardo Alves De Melo <tleonardoam@fastshop.com.br>
2020-12-01 19:14:08 -0300

    augmented no train_df

--- src/augment_data.py
+++ src/augment_data.py
@@ -1,32 +1,30 @@
 import pandas as pd
 import numpy as np
 
-media_path_location = '../data/media_paths_date.csv'
+media_path_location = '../data/train_df.csv'
 
 train_location = '../data/'
 
-paths_df = pd.read_csv(media_path_location)
-new_train_df = paths_df[paths_df.index_path % 4 == 0].copy()
+new_train_df = pd.read_csv(media_path_location)
 model_train_df = new_train_df.copy()
 
-model_train_df.to_parquet('../data/augmented_media_paths_date_0.parquet.gzip', compression='gzip')
+model_train_df.to_csv('../data/augmented_train_0.csv', index=False)
 
-multiply = 2000
+multiply = 50
 
 current_index_path = paths_df.index_path.max() + 1
 
 for mult in range(1, multiply + 1):
 
-	if mult % 100 == 0:
-		print(mult)
-
 	current_df = model_train_df.copy()
 	size = len(current_df.index)
 
-	current_df.date += np.random.normal(0, 60, size)
-	current_df.latitude += np.random.normal(0, 0.00005, size)
-	current_df.longitude += np.random.normal(0, 0.0001, size)
+	for column in range(180):
+		if column % 3 == 0:
+			current_df[column] = current_df[column] + ((current_df[column] != 0)*np.random.normal(0, 60, size))
+		if column % 3 == 1:
+			current_df[column] = current_df[column] + ((current_df[column] != 0)*np.random.normal(0, 0.00005, size))
+		if column % 3 == 2:
+			current_df[column] = current_df[column] + ((current_df[column] != 0)*np.random.normal(0, 0.0001, size))
 
-	current_df.index_path += (mult*current_df.index_path.max())
-
-	current_df.to_parquet('../data/augmented_media_paths_date_' + str(mult) + '.parquet.gzip', compression='gzip')
+	current_df.to_csv('../data/augmented_train_' + str(mult) + '.csv', index=False, header=False)
